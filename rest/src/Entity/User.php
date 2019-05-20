7ffffc13ef70743aa2bd2e4b18d5ee9f0b830479
mateusz Kozioł <mateusz.koziol15@gmail.com>
2019-05-20 22:52:50 +0200

    add full configuration of the oauth server bundle

---
+++ rest/src/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", name="first_name")
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", name="last_name")
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $email;
+}
